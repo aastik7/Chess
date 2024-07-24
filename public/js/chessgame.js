2f5c4223bc3e31caeda1a5e532c9f22a1a0bf17f
Aastik <aastikdude7@gmail.com>
2024-07-24 17:19:57 +0530

    Piece element and board logic

--- public/js/chessgame.js
+++ public/js/chessgame.js
@@ -1,3 +1,48 @@
 const socket = io();
 const chess = new Chess();
 const boardElement = document.querySelector(".chessboard");
+
+let draggedPiece = null;
+let sourceSquare = null;
+let playerRole = null;
+
+const renderBoard = () => {
+  const board = chess.board();
+  boardElement.innerHTML = "";
+  board.forEach((row, rowindex) => {
+    row.forEach((square, squareindex) => {
+      const squareElement = document.createElement("div");
+      squareElement.classList.add(
+        "sqaure",
+        (rowindex + squareindex) % 2 === 0 ? "light" : "dark"
+      );
+
+      squareElement.dataset.row = rowindex;
+      squareElement.dataset.col = square;
+
+      if (square) {
+        const pieceElement = document.createElement("div");
+        pieceElement.classList.add(
+          "piece",
+          square.color === "w" ? "white" : "black"
+        );
+        pieceElement.innerText = "";
+        pieceElement.draggable = playerRole === square.color;
+
+        pieceElement.addEventListener("dragstart", () => {
+          if (pieceElement.draggable) {
+            draggedPiece = pieceElement;
+            sourceSquare = { row: rowindex, col: squareindex };
+            e.dataTransfer.setData("text/plain", ""); //Reducing issues with drag
+          }
+        });
+      }
+    });
+  });
+};
+
+const handleMove = () => {};
+
+const getPieceUnicode = () => {};
+
+renderBoard();
